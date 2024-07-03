09203026859905fc2f98d70ecbf6b64418d25a99
Geetal patel <geetupatel07@gmail.com>
2024-07-03 11:52:51 +0530

    forth commit

--- Reactjs_Projects/src/App.jsx
+++ Reactjs_Projects/src/App.jsx
@@ -20,7 +20,7 @@ import Quizapp from './components/17.Quizapp/Quizapp';
 import NestedComments from './components/18.NestedComments/index';
 import PDFViewer from './components/19.PDFViewer/PDFViewer';
 import FirebaseTodo from './components/20. FirebaseTodo/index';
-import FirebaseAuth from './components/21.FirebaseAuth/FirebaseAuth'
+
 
 
 
@@ -48,7 +48,6 @@ function App() {
       <NestedComments />
       <PDFViewer />
       <FirebaseTodo />
-      <FirebaseAuth />
       
     </div>
   )
